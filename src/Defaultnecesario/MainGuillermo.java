4587a921f2d8d88301ef64d96b88a031b323957b
prueba <Dein@Dein-PC>
2017-03-28 18:57:24 +0200

    Version de darios powah

--- src/Defaultnecesario/MainGuillermo.java
+++ src/Defaultnecesario/MainGuillermo.java
@@ -3,7 +3,8 @@ package Defaultnecesario;
 public class MainGuillermo {
 
 	public static void main(String[] args) {
-		System.out.println("hola guillermo me lees? o ya nsdsdsdsdsdo?¿");    
+		System.out.println("hola guillermo me lees? o ya nsdsdsdsdsdo?¿");   
+		System.out.println("Dario al habla!!");
 
 	}
 
