21f38e4593bf4a9e02b0a5a43e3a3bda1275aa79
Kaykas <Kaykas@192.168.1.34>
2017-03-28 19:09:33 +0200

    Cambiando el habla de guille

--- src/Defaultnecesario/MainGuillermo.java
+++ src/Defaultnecesario/MainGuillermo.java
@@ -5,6 +5,8 @@ public class MainGuillermo {
 	public static void main(String[] args) {
 		System.out.println("hola guillermo me lees? o ya nsdsdsdsdsdo?¿");   
 		System.out.println("Dario al habla!!");
+		System.out.println("el guille primo al habla");
+		
 
 	}
 
