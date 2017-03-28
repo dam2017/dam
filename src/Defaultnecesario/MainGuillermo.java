04d407e7b0045e3a3440d956b9c17bf1306d9478
IrvinLaD <IrvinLaD@IRVINLAD-PC>
2017-03-28 18:16:50 +0200

    Guillermo

--- src/Defaultnecesario/MainGuillermo.java
+++ src/Defaultnecesario/MainGuillermo.java
@@ -3,7 +3,7 @@ package Defaultnecesario;
 public class MainGuillermo {
 
 	public static void main(String[] args) {
-		System.out.println("hola adsasdadsadasdsdsd ");    
+		System.out.println("hola guillermo me lees?");    
 
 	}
 
