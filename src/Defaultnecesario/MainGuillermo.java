238ac7a37f3cba1fde5de1648f6ae3f405811945
Alfonso.prueba <aljorquerafp@gmail.com>
2017-03-31 09:15:39 +0200

    version borrada

--- src/Defaultnecesario/MainGuillermo.java
+++ src/Defaultnecesario/MainGuillermo.java
@@ -3,13 +3,7 @@ package Defaultnecesario;
 public class MainGuillermo {
 
 	public static void main(String[] args) {
-		System.out.println("hola guillermo me lees? o ya nsdsdsdsdsdo?¿");   
-		System.out.println("Dario al habla!!");
-		System.out.println("el guille primo al habla editado por ivan");
 		
-		System.out.println("hola dario me lees, aqui guille");
-		
-		System.out.println("Prueba Alfonso");//comentario correcto
 		System.out.println("probando merge");
 		
 
