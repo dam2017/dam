93a4fce62ec45ee1b7b0c8fdfdd0fffc7f371e2e
alumno <alumno@dam107>
2017-03-31 09:57:09 +0200

    Version Guillermo

--- src/Defaultnecesario/MainGuillermo.java
+++ src/Defaultnecesario/MainGuillermo.java
@@ -3,9 +3,11 @@ package Defaultnecesario;
 public class MainGuillermo {
 
 	public static void main(String[] args) {
-		
+		//borrado aceptado
 		System.out.println("probando merge");
 		
+		
+		//Version Guillermo
 
 	}
 
